1be81ab5ca48f17990f2016b8e7dd8c7265a8e41
mohanbera <immohanbera777@gmail.com>
2023-02-21 09:28:12 +0530

    init commit

---
+++ src/main/java/io/vertx/example/MainVerticle.java
@@ -0,0 +1,79 @@
+package io.vertx.example;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.mysqlclient.MySQLClient;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.SqlClient;
+import io.vertx.sqlclient.SqlConnection;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        SqlClient sqlPool = startDB(vertx);
+        Router router = Router.router(vertx);
+        router.get("/login").handler(routingContext -> {
+
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/plain");
+            response.end("Hello from Vert.x!");
+        });
+
+        router.get("/test").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            // response.putHeader("content-type", "application/json");
+            // response.end("Hello from Vert.x!");
+            getAllData(routingContext, sqlPool);
+        });
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
+
+    }
+
+    public SqlClient startDB(Vertx vertx) {
+        MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+                .setPort(3306)
+                .setHost("34.93.176.252")
+                .setDatabase("note_db")
+                .setUser("root")
+                .setPassword("Laltu@777");
+
+        // Pool options
+        PoolOptions poolOptions = new PoolOptions()
+                .setMaxSize(2);
+
+        // Create the client pool
+        return MySQLPool.client(vertx, connectOptions, poolOptions);
+    }
+
+    private void getAllData(final RoutingContext routingContext, SqlClient client) {
+        client
+                .query("SELECT * FROM users")
+                .execute(ar -> {
+                    if (ar.succeeded()) {
+                        RowSet<Row> result = ar.result();
+                        System.out.println("Got " + result.size() + " rows ");
+                    } else {
+                        System.out.println("Failure: " + ar.cause().getMessage());
+                    }
+
+                    // Now close the pool
+                    client.close();
+                });
+    }
+}
---
+++ src/main/java/io/vertx/example/models/User.java
@@ -0,0 +1,3 @@
+package io.vertx.example.models;
+
+public record User (int uid, String username, String name, String email, String code, String profile_pic) {}
